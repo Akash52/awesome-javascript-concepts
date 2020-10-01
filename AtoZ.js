e85c71de9094c3a8e8732ba77107b2089769f9fb
Akash52 <ac8572611@gmail.com>
2020-10-01 16:37:49 +0530

    JS

---
+++ AtoZ.js
@@ -0,0 +1,11 @@
+//Log to consloe 
+console.log('Hello world');
+console.log(123);
+console.log(true)
+var greeting='Hello';
+console.log(greeting);
+console.log(1,2,3,4);
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+
+console.error("This is some errror");
