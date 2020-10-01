6a19453af4836d0b819881ab80f68445b6087e8d
Akash52 <ac8572611@gmail.com>
2020-10-01 17:01:47 +0530

    JS

--- AtoZ.js
+++ AtoZ.js
@@ -7,5 +7,19 @@ console.log(greeting);
 console.log(1,2,3,4);
 console.log({a:1,b:2});
 console.log({a:1,b:2});
-
 console.error("This is some errror");
+console.clear();
+console.warn();
+console.time('Hello'); //Function start
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world'); //Display script taken time
+console.log('Hello world');
+console.log('Hello world');
+console.timeEnd('Hello'); //Function End
+
+/*
+multi
+line
+comments
+*/
