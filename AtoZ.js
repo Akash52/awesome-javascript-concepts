89a7cb7e95f7c40c039089383b6d7f230997b327
Akash52 <ac8572611@gmail.com>
2020-10-05 07:42:04 +0530

    Type convesion .

--- AtoZ.js
+++ AtoZ.js
@@ -1,13 +1,12 @@
-/*
 //Log to consloe 
 console.log('Hello world');
 console.log(123);
 console.log(true)
-var greeting='Hello';
+var greeting = 'Hello';
 console.log(greeting);
-console.log(1,2,3,4);
-console.log({a:1,b:2});
-console.log({a:1,b:2});
+console.log(1, 2, 3, 4);
+console.log({ a: 1, b: 2 });
+console.log({ a: 1, b: 2 });
 console.error("This is some errror");
 console.clear();
 console.warn();
@@ -28,60 +27,61 @@ comments
 //DataType,Naming,Rules
 //var,let,const
 
-var name="Akash Doe";
+var name = "Akash Doe";
 console.log(name);
-name="Steave smith"; //Re-assing variable
+name = "Steave smith"; //Re-assing variable
 console.log(name);
 
 //Initialize variable
 
 var greeting;
 console.log(greeting); //undefined error
-greeting='Hello';
+greeting = 'Hello';
 console.log(greeting);
 
 //Letters,Numbers,_ string ,variable rules
 Can not start with number
-var 1name='John'; //Return error
-var _name='John'; //these is valid
+var 1 name = 'John'; //Return error
+var _name = 'John'; //these is valid
 
 //Multi word vars
 
-var firtsName='Akash'; //Camel case
-var first_name='Sara';  //Underscore
-var FirstName='Tome';   // Pascal case
+var firtsName = 'Akash'; //Camel case
+var first_name = 'Sara'; //Underscore
+var FirstName = 'Tome'; // Pascal case
 var firstname; //Not recomneded 
 
 //LET
 let name;
-let name="Akash Doe";
+let name = "Akash Doe";
 console.log(name);
-name="Steave smith"; //Re-assing variable
+name = "Steave smith"; //Re-assing variable
 console.log(name);
 
 //Const
 
-const name="Akash Doe";
+const name = "Akash Doe";
 console.log(name);
-name="Steave smith"; // can't Re-assing variable
+name = "Steave smith"; // can't Re-assing variable
 const greeting;
 
 Const
-const Person={
+const Person = {
     name: 'Akash',
-    age : '20'
+    age: '20'
 }
-Person.name='Sara';
-Person.age='30';
+Person.name = 'Sara';
+Person.age = '30';
 
 console.log(Person);
 
-const numbers=[1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
 numbers.push(6);
 
 console.log(numbers);
 
-*/
+*
+/
 
 
 //Data type
@@ -124,3 +124,52 @@ console.log(typeof hobbies);
 console.log(address);
 const today = new Date();
 console.log(today);
+
+//Type conversion
+
+let val;
+//Number to string
+val = String(555);
+val = String(4 + 4);
+//Bool to string
+val = String(true);
+//Date to string
+val = String(new Date());
+//Array to string
+val = String([1, 2, 3, 4]);
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+//Stirng to Number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3, 4, 5]);
+
+
+val = parseInt('100'); //Only return integer
+val = parseFloat('100.4646'); //Return Float value
+
+
+//String concersion
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
+
+
+
+
+
+
+
+//Output 
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed());
