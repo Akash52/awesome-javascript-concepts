8a5288f9c23dcfa35bbc1a5dc3319d26f59ef2c9
Akash52 <ac8572611@gmail.com>
2020-10-21 21:11:46 +0530

    Canvas

---
+++ Canvas/app.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//Fill react
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle = "blue";
+ctx.fillRect(200, 20, 150, 100);
+ctx.fillStyle = "green";
+ctx.fillRect(400, 20, 150, 100);
