80f097b235b33c8f7431d0904063cecfe875bfb7
Akash52 <ac8572611@gmail.com>
2020-10-21 21:23:27 +0530

    fill text done

--- Canvas/app.js
+++ Canvas/app.js
@@ -6,7 +6,7 @@ ctx.fillStyle = "red";
 ctx.fillRect(20, 20, 150, 100);
 ctx.fillStyle = "blue";
 ctx.fillRect(200, 20, 150, 100);
-ctx.fillStyle = "green";
+ctx.fillStyle = "white";
 ctx.fillRect(400, 20, 150, 100);
 //StrokeReact()
 
@@ -16,3 +16,8 @@ ctx.strokeRect(100, 200, 150, 100);
 
 //ClearRect()
 ctx.clearRect(25, 25, 140, 90);
+
+//Filltext()
+ctx.font = "25px Arial";
+ctx.fillStyle = "purple";
+ctx.fillText("Hello World", 400, 100);
