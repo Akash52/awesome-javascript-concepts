d5f94ff951c1b71eca77f8e92bb3ebcc0041e38d
Akash52 <ac8572611@gmail.com>
2020-10-21 21:18:03 +0530

    Canvas rect,storke & clear done

--- Canvas/app.js
+++ Canvas/app.js
@@ -8,3 +8,11 @@ ctx.fillStyle = "blue";
 ctx.fillRect(200, 20, 150, 100);
 ctx.fillStyle = "green";
 ctx.fillRect(400, 20, 150, 100);
+//StrokeReact()
+
+ctx.linewidth = 5;
+ctx.strokestyle = "green";
+ctx.strokeRect(100, 200, 150, 100);
+
+//ClearRect()
+ctx.clearRect(25, 25, 140, 90);
