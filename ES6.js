e97795b3c9d18bf84d6459707ee2a521dfac7439
Akash52 <ac8572611@gmail.com>
2021-01-12 22:33:46 +0530

    forEach in Sum of Array

--- ES6.js
+++ ES6.js
@@ -8,3 +8,21 @@ let colors = ['Red', 'Blue', 'Green']
 colors.forEach(function (color) {
   console.log(color)
 })
+
+//Creat an array of numbers
+
+let numbers = [1, 2, 3, 4, 5]
+
+//Create a Variable to hold them
+
+let sum = 0
+
+//Loop over the array, icreamenting the sum variables
+
+numbers.forEach(function (number) {
+  sum += number
+})
+
+// print the sum of varaible
+
+console.log(sum)
