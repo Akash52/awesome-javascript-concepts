9181c558b21118f3bd0fdd2f89d322fea4e56fa3
Akash52 <ac8572611@gmail.com>
2021-01-12 22:12:50 +0530

    Array Helper Method

---
+++ ES6.js
@@ -0,0 +1,11 @@
+//Array helper Method
+
+// forEach, map, filter, find, every, some & Reduce
+
+//1. forEach
+
+let colors = ['Red', 'Blue', 'Green']
+
+colors.forEach(function (color) {
+  console.log(color)
+})
