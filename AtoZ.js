b232b3e9114cbc111a382d7361a2ac1ffd75760a
Akash52 <ac8572611@gmail.com>
2021-04-04 15:59:29 +0530

    Getting data from an External API

--- AtoZ.js
+++ AtoZ.js
@@ -775,7 +775,7 @@ function loadData() {
 
   xhr.send()
 }
-*/
+
 
 //Working with AJAX & JSON
 
@@ -834,4 +834,39 @@ function loadCustombers(e) {
     }
   }
   xhr.send()
+}
+*/
+
+// Getting data from an External API
+
+document.querySelector('.get-jokes').addEventListener('click', getJokes)
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value
+
+  const xhr = new XMLHttpRequest()
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true)
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText)
+
+      let output = ''
+
+      if (response.type === 'success') {
+        response.value.forEach(function (joke) {
+          output += `<li>${joke.joke}</li>`
+        })
+      } else {
+        output += '<li>Something went wrong</li>'
+      }
+
+      document.querySelector('.jokes').innerHTML = output
+    }
+  }
+
+  xhr.send()
+
+  e.preventDefault()
 }
