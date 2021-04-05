a9a8a24a70596133cfc2dd07de508b943cfb4f1a
Akash52 <ac8572611@gmail.com>
2021-04-05 22:26:24 +0530

    Callback Function

--- AtoZ.js
+++ AtoZ.js
@@ -835,7 +835,7 @@ function loadCustombers(e) {
   }
   xhr.send()
 }
-*/
+
 
 // Getting data from an External API
 
@@ -870,3 +870,29 @@ function getJokes(e) {
 
   e.preventDefault()
 }
+*/
+
+// Callbak Funtion
+const posts = [
+  { title: 'Post One', body: 'this is post one' },
+  { title: 'Post Two', body: 'this is post two' }
+]
+
+function createPost(post, callback) {
+  setTimeout(function () {
+    posts.push(post)
+    callback()
+  }, 2000)
+}
+
+function getPosts() {
+  setTimeout(function () {
+    let output = ''
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`
+    })
+    document.body.innerHTML = output
+  }, 1000)
+}
+
+createPost({ title: 'Post Three', body: 'This is post three'{title:'Post One',body:'this is post one'}, getPosts })
