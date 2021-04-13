47d352cbe3a4014459d8121d3bd4e3932405dbba
Akash52 <ac8572611@gmail.com>
2021-04-13 13:26:34 +0530

    1.Variables, Constants & Comments

---
+++ Net Ninja JS/Vanilla/script.js
@@ -0,0 +1,15 @@
+//1.Variables, Constants & Comments
+let age = 25
+let year = 2021
+console.log(age, year)
+
+age = 30
+console.log(age)
+
+const points = 100
+/* points = 50 ->We can't re-assign varialbe */
+console.log(points)
+
+/* Old way creating variable */
+var score = 75
+console.log(score)
