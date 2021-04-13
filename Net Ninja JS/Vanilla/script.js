97061241361b516b929dc7506d879e1031558b5c
Akash52 <ac8572611@gmail.com>
2021-04-13 13:40:52 +0530

    2.Strings

--- Net Ninja JS/Vanilla/script.js
+++ Net Ninja JS/Vanilla/script.js
@@ -13,3 +13,32 @@ console.log(points)
 /* Old way creating variable */
 var score = 75
 console.log(score)
+
+//2.Strings
+console.log('Akash Chauhan')
+
+let email = 'abc@gmail.com'
+console.log(email)
+
+// string concatenation
+let firsName = 'Jhone'
+let lastName = 'Doe'
+
+let fullname = firsName + ' ' + lastName
+console.log(fullname)
+
+// getting characters
+
+console.log(fullname[0])
+
+// string length
+
+console.log(fullname.length)
+
+// string methods
+
+console.log(fullname.toUpperCase())
+console.log(fullname.toLowerCase())
+
+let index = email.indexOf('@')
+console.log(index)
