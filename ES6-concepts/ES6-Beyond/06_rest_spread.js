db16d937bddae72cc6a81214080c27bdf8c09568
Akash52 <ac8572611@gmail.com>
2022-05-05 19:15:45 +0530

    Rest and Spread operators are in ES6

---
+++ ES6-concepts/ES6-Beyond/06_rest_spread.js
@@ -0,0 +1,47 @@
+//Rest Paramenter alwasy be in last place
+
+// function sum(a, b, ...values) {
+//   let sum = a + b;
+//   values.forEach((value) => (sum += value));
+//   return sum;
+// }
+// console.log(sum(2, 4, 2, 3, 5));
+
+// const user = {
+//   name: 'John',
+//   age: 30,
+// };
+
+// const cart = {
+//   items: [
+//     {
+//       product: {
+//         name: 'Milk',
+//         price: 3.5,
+//       },
+//       quantity: 2,
+//     },
+//     {
+//       product: {
+//         name: 'Bread',
+//         price: 2.5,
+//       },
+//       quantity: 3,
+//     },
+//   ],
+// };
+
+// const deliveryAddress = {
+//   street: 'Main Street',
+//   city: 'New York',
+//   state: 'NY',
+//   zipCode: '10001',
+// };
+
+// const order = {
+//   ...user,
+//   ...cart.items.product,
+//   ...deliveryAddress,
+// };
+
+// console.log(order);
