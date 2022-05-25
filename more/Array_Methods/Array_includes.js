3533b5bfc5329e5045aee46a693af930163ca19f
Akash52 <ac8572611@gmail.com>
2022-05-25 22:28:48 +0530

    Array_includes

---
+++ more/Array_Methods/Array_includes.js
@@ -0,0 +1,12 @@
+/*
+This method will return true if the array contains a certain element, 
+and false if not.
+*/
+
+//Example 1
+
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
+
+console.log(fruits.includes('Apple')) // true
+console.log(fruits.includes('Orange')) // true
+console.log(fruits.includes('Pineapple')) // false
