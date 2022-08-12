2b6368edd72b58a5595325fd23498e2c6ff4b023
Akash52 <ac8572611@gmail.com>
2022-08-12 16:29:39 +0530

    javascript-create-a-promise-chain-in-javascript-with-promise-then

---
+++ ES6-concepts/ES6-Beyond/08_Promise/script.js
@@ -0,0 +1,48 @@
+const API_URL = 'https://starwars.egghead.training/films';
+const output = document.getElementById('output');
+const body = document.querySelector('body');
+
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    output.innerText = getFilmTitle(data);
+  })
+  .catch(handleError);
+
+function getFilmTitle(data) {
+  return data
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map((film) => `${film.episode_id}. ${film.title}`)
+    .join('\n');
+}
+
+function handleError(error) {
+  output.innerText = 'Error';
+  console.error(error);
+}
+
+// const films = fetch(API_URL);
+// films
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const filmData = data.map(
+//       (film) => `${film.title} (${film.release_date}) (${film.episode_id})`
+//     );
+//     const filmList = document.createElement('ul');
+//     filmList.innerHTML = filmData.map((film) => `<li>${film}</li>`).join('');
+//     document.body.appendChild(filmList);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log('finally');
+//   })
+//   .then(() => {
+//     console.log('then');
+//   });
+
+body.style.backgroundColor = '#000';
+body.style.color = '#fff';
+body.style.fontSize = '1.5rem';
+body.style.fontFamily = 'monospace';
