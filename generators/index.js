6a7e384cd4f5e24aa691b8cfc58fa2cfade366f7
Akash52 <ac8572611@gmail.com>
2022-08-24 12:32:06 +0530

    create-your-first-iterator-in-javascript

--- generators/index.js
+++ generators/index.js
@@ -19,3 +19,27 @@ const abc = iterator2();
 for (let i of abc) {
   console.log(i);
 }
+
+//Create first iterator
+
+let i = 0;
+
+const next = () => ({
+  value: i++,
+  done: i > 10,
+});
+
+console.log(next());
+console.log(next());
+
+const iterator3 = {
+  [Symbol.iterator]() {
+    return {
+      next,
+    };
+  },
+};
+
+for (let value of iterator3) {
+  console.log(value);
+}
