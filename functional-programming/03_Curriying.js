e47c8eda3fa9252d71e145e034938ed58b10e518
Akash52 <ac8572611@gmail.com>
2022-09-06 01:11:18 +0530

    refactor-a-function-to-use-currying-in-javascript

---
+++ functional-programming/03_Curriying.js
@@ -0,0 +1,19 @@
+//Currying
+// Currying simply means evaluating functions with multiple arguments and decomposing them into a sequence of functions with a single argument.
+
+// function add(x) {
+//   return function (y) {
+//     return x + y;
+//   };
+// }
+
+// const addThree = add(3);
+// console.log(addThree(4));
+// console.log(addThree(6));
+
+//Arity
+
+// 1 = unary -> Recieve one arguments
+// 2 = binary -> Recieve two arguments
+// 3 = ternary -> Recieve three arguments
+// 4 = quaternary -> Recieve four arguments
