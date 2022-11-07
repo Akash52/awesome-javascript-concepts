eb1c91e8c77eb5025f001c2ad4cb79f7413634f9
Akash52 <ac8572611@gmail.com>
2022-11-07 16:58:59 +0530

    Imperative vs Declarative Programming

--- javascript-masterclass-master/src/index.js
+++ javascript-masterclass-master/src/index.js
@@ -23,62 +23,91 @@ import "../assets/css/style.css";
 
 //Immutable Data Strucuture Pattern
 
+// const items = [
+//   { id: "🍔", name: "Super Burger", price: 399 },
+//   { id: "🍟", name: "Jumbo Fries", price: 199 },
+//   { id: "🥤", name: "Big Slurp", price: 299 },
+// ];
+
+// console.log(items);
+
+//array - add
+
+// const newItem = {
+//   id: "🍎",
+//   name: "Apple",
+//   price: 299,
+// };
+// items.push(newItem);
+// const newItems = [...items, newItem];
+// console.log(items);
+// console.log(newItems);
+
+//array - remove
+
+// const removed = items.splice(0, 1);
+// console.log(removed, items);
+// const updatedItems = items.filter((item) => item.id !== "🍔");
+// console.log(updatedItems, items);
+
+//Objects - add
+
+// const item = {
+//   id: "🍎",
+//   name: "Apple",
+// };
+// const itemThatIsNew = {
+//   ...item,
+//   price: 244,
+// };
+// // item.price = 299;
+// console.log(item, itemThatIsNew);
+
+//Objects - remove
+
+// const itemToRemove = {
+//   id: "🍎",
+//   name: "Apple",
+//   price: 244,
+// };
+// delete itemToRemove.price;
+// console.log(itemToRemove);
+// const { price, ...leftOverItems } = itemToRemove;
+// console.log(price, itemToRemove);
+// console.log(leftOverItems);
+
+//identity
+
+// console.log({} === {});
+// console.log([] === []);
+// console.log(itemToRemove === itemToRemove);
+// console.log(itemToRemove === leftOverItems);
+
+//Imperative vs Declarative Programming
+
 const items = [
   { id: "🍔", name: "Super Burger", price: 399 },
   { id: "🍟", name: "Jumbo Fries", price: 199 },
   { id: "🥤", name: "Big Slurp", price: 299 },
 ];
 
+//Imperative
+
+const itemNamesImperative = [];
+
+for (let i = 0; i < items.length; i++) {
+  const item = items[i];
+  itemNamesImperative.push(item);
+}
 console.log(items);
+console.log(itemNamesImperative);
 
-//array - add
-
-const newItem = {
-  id: "🍎",
-  name: "Apple",
-  price: 299,
-};
-// items.push(newItem);
-const newItems = [...items, newItem];
-console.log(items);
-console.log(newItems);
-
-//array - remove
-
-// const removed = items.splice(0, 1);
-// console.log(removed, items);
-const updatedItems = items.filter((item) => item.id !== "🍔");
-console.log(updatedItems, items);
-
-//Objects - add
-
-const item = {
-  id: "🍎",
-  name: "Apple",
-};
-const itemThatIsNew = {
-  ...item,
-  price: 244,
-};
-// item.price = 299;
-console.log(item, itemThatIsNew);
-
-//Objects - remove
-
-const itemToRemove = {
-  id: "🍎",
-  name: "Apple",
-  price: 244,
-};
-// delete itemToRemove.price;
-console.log(itemToRemove);
-const { price, ...leftOverItems } = itemToRemove;
-console.log(price, itemToRemove);
-console.log(leftOverItems);
-
-//identity
-
-console.log({} === {});
-console.log([] === []);
-console.log(itemToRemove === itemToRemove);
-console.log(itemToRemove === leftOverItems);
+// Declarative
+const itemNamesDeclarative = items
+  .map(function (item) {
+    return item.price;
+  })
+  .reduce(function (price, nextPrice) {
+    return price + nextPrice;
+  });
+console.log(itemNamesDeclarative);
