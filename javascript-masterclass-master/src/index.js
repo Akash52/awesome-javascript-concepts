63ed6a29138b07f78697948d894906e4e41df3ef
Akash52 <ac8572611@gmail.com>
2022-11-07 14:37:57 +0530

    Immutable vs Mutable Concepts

--- javascript-masterclass-master/src/index.js
+++ javascript-masterclass-master/src/index.js
@@ -1,9 +1,22 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const items = [
-  { id: '🍔', name: 'Super Burger', price: 399 },
-  { id: '🍟', name: 'Jumbo Fries', price: 199 },
-  { id: '🥤', name: 'Big Slurp', price: 299 },
-];
+// const items = [
+//   { id: '🍔', name: 'Super Burger', price: 399 },
+//   { id: '🍟', name: 'Jumbo Fries', price: 199 },
+//   { id: '🥤', name: 'Big Slurp', price: 299 },
+// ];
 
-console.log(items);
+// console.log(items);
+
+//Immutable vs Mutable Concepts
+
+let a = "Apple Sky";
+a = "Sky Apple";
+
+const b = a.slice(0, 1);
+console.log(a, b);
+
+const x = Object.freeze({ id: "🍔", name: "Super Burger", price: 399 });
+
+x.id = "😃";
+console.log(x);
