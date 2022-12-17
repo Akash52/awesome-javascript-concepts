1d430128387ecd18b0174e51a5c2a9c64654adb6
Akash52 <ac8572611@gmail.com>
2022-12-17 18:26:54 +0530

    currying & partial Application

--- javascript-masterclass/README.md
+++ javascript-masterclass/README.md
@@ -191,7 +191,7 @@ console.log(getFires(items));
 console.log(getBurger(items));
 ```
 
-### Higher-Oreder Functions
+### Higher-Order Functions
 
 ```js
 const items = [
@@ -214,5 +214,37 @@ const getFires = getNameFromId("🍟");
 const getBurger = getNameFromId("🍔");
 
 console.log(getFires(items));
+console.log(getBurger(items));
+```
+
+### currying & partial Application
+
+```js
+const items = [
+  { id: "🍔", name: "Super Burger", price: 399 },
+  { id: "🍟", name: "Jumbo Fries", price: 199 },
+  { id: "🥤", name: "Big Slurp", price: 299 },
+];
+
+//f(a,b,c)
+//f(a)(b)(c)
+
+const curry = (fn) => {
+  return (...args) => {
+    if (args.length >= fn.length) {
+      return fn.apply(null, args);
+    }
+    return fn.bind(null, ...args);
+  };
+};
+
+const getNameFromId = curry(
+  (id, items) => items.find((item) => item.id === id).name
+);
+
+const getFires = getNameFromId("🍟", items);
+const getBurger = getNameFromId("🍔"); //partially applying
+
+console.log(getFires);
 console.log(getBurger(items));
 ```
--- javascript-masterclass/src/index.js
+++ javascript-masterclass/src/index.js
@@ -1,189 +1,5 @@
 import "../assets/css/style.css";
-
-// const items = [
-//   { id: '🍔', name: 'Super Burger', price: 399 },
-//   { id: '🍟', name: 'Jumbo Fries', price: 199 },
-//   { id: '🥤', name: 'Big Slurp', price: 299 },
-// ];
-
-// console.log(items);
-
-// Immutable vs Mutable Concepts
-
-// let a = "Apple Sky";
-// a = "Sky Apple";
-
-// const b = a.slice(0, 1);
-// console.log(a, b);
-
-// const x = Object.freeze({ id: "🍔", name: "Super Burger", price: 399 });
-
-// x.id = "😃";
-// console.log(x);
-
-// Immutable Data Strucuture Pattern
-
-// const items = [
-//   { id: "🍔", name: "Super Burger", price: 399 },
-//   { id: "🍟", name: "Jumbo Fries", price: 199 },
-//   { id: "🥤", name: "Big Slurp", price: 299 },
-// ];
-
-// console.log(items);
-
-// array - add
-
-// const newItem = {
-//   id: "🍎",
-//   name: "Apple",
-//   price: 299,
-// };
-// items.push(newItem);
-// const newItems = [...items, newItem];
-// console.log(items);
-// console.log(newItems);
-
-// array - remove
-
-// const removed = items.splice(0, 1);
-// console.log(removed, items);
-// const updatedItems = items.filter((item) => item.id !== "🍔");
-// console.log(updatedItems, items);
-
-// Objects - add
-
-// const item = {
-//   id: "🍎",
-//   name: "Apple",
-// };
-// const itemThatIsNew = {
-//   ...item,
-//   price: 244,
-// };
-// // item.price = 299;
-// console.log(item, itemThatIsNew);
-
-// Objects - remove
-
-// const itemToRemove = {
-//   id: "🍎",
-//   name: "Apple",
-//   price: 244,
-// };
-// delete itemToRemove.price;
-// console.log(itemToRemove);
-// const { price, ...leftOverItems } = itemToRemove;
-// console.log(price, itemToRemove);
-// console.log(leftOverItems);
-
-// identity
-
-// console.log({} === {});
-// console.log([] === []);
-// console.log(itemToRemove === itemToRemove);
-// console.log(itemToRemove === leftOverItems);
-
-// Imperative vs Declarative Programming
-
-// const items = [
-//   { id: "🍔", name: "Super Burger", price: 399 },
-//   { id: "🍟", name: "Jumbo Fries", price: 199 },
-//   { id: "🥤", name: "Big Slurp", price: 299 },
-// ];
-
-// Imperative
-
-// const itemNamesImperative = [];
-
-// for (let i = 0; i < items.length; i++) {
-//   const item = items[i];
-//   itemNamesImperative.push(item);
-// }
-// console.log(items);
-// console.log(itemNamesImperative);
-
-// Declarative
-// const itemNamesDeclarative = items
-//   .map(function (item) {
-//     return item.price;
-//   })
-//   .reduce(function (price, nextPrice) {
-//     return price + nextPrice;
-//   });
-// console.log(itemNamesDeclarative);
-
-// Lambda Expressions VS Anonymous Functions
-
-// const items = [
-//   { id: "🍔", name: "Super Burger", price: 399 },
-//   { id: "🍟", name: "Jumbo Fries", price: 199 },
-//   { id: "🥤", name: "Big Slurp", price: 299 },
-// ];
-
-// function getItemName(item) {
-//   return item.name;
-// }
-// console.log(items.map(getItemName));
-
-// console.log(
-//   items.map(function (item) {
-//     return item.name;
-//   })
-// );
-
-// console.log(items.map((item) => item.name));
-
-// const getItemNameExp = (item) => item.name;
-// console.log(items.map(getItemNameExp));
-
-// Pure Functions and Referential Transparency
-
-// Pure Funcgtions ?
-// 1. Referential transparency
-// 2. Side-Effect free
-
-// const items = [
-//   { id: "🍔", name: "Super Burger", price: 399 },
-//   { id: "🍟", name: "Jumbo Fries", price: 199 },
-//   { id: "🥤", name: "Big Slurp", price: 299 },
-// ];
-
-// ImPure function
-
-// const getTotalImpure = () => {
-//   document.querySelector("#app").innerHTML = items.reduce(
-//     (x, y) => x + y.price,
-//     0
-//   );
-// };
-
-// getTotalImpure();
-
-// Pure function
-
-// const getTotalPure = (v) => v.reduce((x, y) => x + y.price, 0);
-// document.querySelector("#app").innerHTML = getTotalPure(items);
-
-// Funtion Clousers
-
-// const items = [
-//   { id: "🍔", name: "Super Burger", price: 399 },
-//   { id: "🍟", name: "Jumbo Fries", price: 199 },
-//   { id: "🥤", name: "Big Slurp", price: 299 },
-// ];
-
-// const getNameFromId = (id) => {
-//   return (items) => {
-//     return items.find((item) => item.id === id).name;
-//   };
-// };
-
-// const getFires = getNameFromId("🍟");
-// const getBurger = getNameFromId("🍔");
-// console.log(getFires(items));
-// console.log(getBurger(items));
-
-//Higher-Oreder Functions
+//currying
 
 const items = [
   { id: "🍔", name: "Super Burger", price: 399 },
@@ -191,18 +7,24 @@ const items = [
   { id: "🥤", name: "Big Slurp", price: 299 },
 ];
 
-//HOF
-// 1. Return a new Function
-// 2. take other functions as arguments
+//f(a,b,c)
+//f(a)(b)(c)
 
-const getNameFromId = (id) => {
-  return (items) => {
-    return items.find((item) => item.id === id).name;
+const curry = (fn) => {
+  return (...args) => {
+    if (args.length >= fn.length) {
+      return fn.apply(null, args);
+    }
+    return fn.bind(null, ...args);
   };
 };
 
-const getFires = getNameFromId("🍟");
-const getBurger = getNameFromId("🍔");
+const getNameFromId = curry(
+  (id, items) => items.find((item) => item.id === id).name
+);
 
-console.log(getFires(items));
+const getFires = getNameFromId("🍟", items);
+const getBurger = getNameFromId("🍔"); //partially applying
+
+console.log(getFires);
 console.log(getBurger(items));
